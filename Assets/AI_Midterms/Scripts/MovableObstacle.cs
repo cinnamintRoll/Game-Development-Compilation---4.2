e766f58d06120e8e5a332039ed0b60bf174ebfae
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2025-03-04 21:40:17 +0800

    Midterm AI

---
+++ Assets/AI_Midterms/Scripts/MovableObstacle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MovableObstacle : MonoBehaviour
+{
+      [SerializeField] private float speedForward;
+    //[SerializeField] private float speedBackward;
+
+    private bool isSwitchDir;
+    
+    void Update()
+    {
+        if (!isSwitchDir) 
+        {
+            transform.Translate(Vector3.forward * speedForward * Time.deltaTime);
+        }
+
+        else 
+        {
+            transform.Translate(Vector3.back * speedForward * Time.deltaTime);
+        }
+
+        if (transform.position.z >= 2) 
+        {
+            isSwitchDir = true;
+        }
+        else if (transform.position.z <= -2) 
+        {
+            isSwitchDir= false;
+        }
+    }
+}
